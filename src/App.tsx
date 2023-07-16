daacbafdcc9c2c82cdbbaa14d64777f9a288205e
Rob Park <robert.park@4legssoftware.com>
2023-07-16 18:39:16 -0400

    add another example test from test-driving a component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Accordion from "./Accordion.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,9 +26,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Accordion title={'Testing'}>
+        <h4>Content</h4>
+      </Accordion>
     </>
   )
 }
---
+++ test/App.test.tsx
@@ -0,0 +1,12 @@
+import {describe, expect, test} from 'vitest';
+import {render, screen} from '@testing-library/react';
+import Accordion from '../src/Accordion';
+import App from "../src/App";
+
+describe("App", () => {
+  test("Should show Accordion", () => {
+    render(<App></App>);
+
+    expect(screen.getByText(/Testing/i)).toBeDefined()
+  })
+})
