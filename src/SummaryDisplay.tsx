efd1e2d4bc153c9117c2ccbc8fae51bbe1142acb
Rob Park <robert.park@4legssoftware.com>
2023-07-29 09:36:07 -0400

    test-drive summary display component

--- src/SummaryDisplay.tsx
+++ src/SummaryDisplay.tsx
@@ -1,14 +1,33 @@
 type SummaryDisplayProps = {
-  data: {}
+  data: [{
+    name: string;
+    amount: number;
+    percent: number;
+  }]
 }
 
-export const SummaryDisplay = (_props: SummaryDisplayProps) => {
-  // const {title, children} = props;
+export const SummaryDisplay = (props: SummaryDisplayProps) => {
+  const {data} = props;
   return (
-    <div>
-      {/*<h1>Simple Table Example</h1>*/}
-      {/*<SimpleTable data={data} />*/}
-    </div>
-  );};
+    <table>
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Amount</th>
+        <th>Percent</th>
+      </tr>
+      </thead>
+      <tbody>
+      {data.map((row) => (
+        <tr key={row.name}>
+          <td>{row.name}</td>
+          <td>{row.amount}</td>
+          <td>{row.percent}</td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default SummaryDisplay;
--- test/SummaryDisplay.test.tsx
+++ test/SummaryDisplay.test.tsx
@@ -1,30 +1,22 @@
-// import React from 'react';
-import {render} from '@testing-library/react';
-import {describe, expect, test} from 'vitest';
+import {render, screen} from '@testing-library/react';
+import {describe, expect, it} from 'vitest';
 import SummaryDisplay from "../src/SummaryDisplay";
-
-// import SimpleTable from './SimpleTable';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('Summary Display', () => {
-  test('renders the table component without errors', () => {
+  it('renders the table component without errors', () => {
     render(<SummaryDisplay data={[]}/>);
   });
+
+  it('renders the correct data in the table', () => {
+    const data = [
+      {name: 'Stock', amount: 7000, percent: 70},
+      {name: 'Cash', amount: 3000, percent: 30},
+    ];
+
+    const {getByText} = render(<SummaryDisplay data={data}/>);
+
+    expect(getByText('Stock')).toBeInTheDocument();
+    // expect(screen.getByText(/Stock/i)).toBeDefined();
+  });
 });
-//
-// // Test case 2: Check if the table renders the correct data
-// test('renders the correct data in the table', () => {
-//   const data = [
-//     { id: 1, name: 'John', age: 30 },
-//     { id: 2, name: 'Jane', age: 25 },
-//   ];
-//
-//   const { getByText } = render(<SimpleTable data={data} />);
-//
-//   // Assert that the data is rendered correctly in the table
-//   expect(getByText('1')).toBeInTheDocument();
-//   expect(getByText('John')).toBeInTheDocument();
-//   expect(getByText('30')).toBeInTheDocument();
-//   expect(getByText('2')).toBeInTheDocument();
-//   expect(getByText('Jane')).toBeInTheDocument();
-//   expect(getByText('25')).toBeInTheDocument();
-// });
